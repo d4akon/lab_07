8e8ea0ef5e57a2a50ffe4eda87da3b6d7b3f77bb
Szymon Maj <szymon.maj@icloud.com>
2022-06-18 01:00:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
